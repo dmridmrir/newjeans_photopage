3f3355d37d1bd86735753d990fc47e18bea4d3d8
dmridmrir <122187770+dmridmrir@users.noreply.github.com>
2023-09-02 15:45:45 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,10 +1,26 @@
 const popupOverlay = document.querySelector('.popup-overlay');
-const popupImage = document.querySelector('.popup-image');
+const popupImage1 = document.querySelector('.popup-image1');
+const popupImage2 = document.querySelector('.popup-image2');
+const popupImage3 = document.querySelector('.popup-image3');
+const popupImage4 = document.querySelector('.popup-image4');
+const popupImage5 = document.querySelector('.popup-image5');
 const closeBtn = document.querySelector('.close-button');
 
-popupImage.addEventListener('click', () => {
-  popupOverlay.classList.add('active');
-});
+  popupImage1.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+  });
+  popupImage2.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+  });
+  popupImage3.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+  });
+  popupImage4.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+  });
+  popupImage5.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+  });
 
 closeBtn.addEventListener('click', () => {
   popupOverlay.classList.remove('active');
